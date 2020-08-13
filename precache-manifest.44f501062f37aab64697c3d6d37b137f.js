22e76e64b361d7fccbb2b775586eccc21eb5ccb6
Isak <isakulstrup@gmail.com>
2020-08-13 08:27:10 +0200

    vue-rpg build

---
+++ precache-manifest.44f501062f37aab64697c3d6d37b137f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b92c75d79b94db1c8aa",
+    "url": "/css/app.b00a1560.css"
+  },
+  {
+    "revision": "7292188ef59daf0171b58a6366d39373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b92c75d79b94db1c8aa",
+    "url": "/js/app.27aaeae9.js"
+  },
+  {
+    "revision": "e314360fa1f1ec2090b4",
+    "url": "/js/chunk-vendors.368d30c8.js"
+  },
+  {
+    "revision": "a6e50b4cc7b8364cad7a8cfdda85599d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
